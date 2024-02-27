f7730a35e4cac18ab0e585989c1b95e7aab66b39
VadimSVV <korsarishe@gmail.com>
2024-02-27 20:32:23 +0300

    Update

--- user_id.php
+++ user_id.php
@@ -1,45 +1,45 @@
 <?php 
 
-//Проверяю наличие параметра user_id в URL
+//Проверка наличия параметра user_id в URL
 if (!isset($_GET['user_id'])) {
-    // Возвращаю ошибку, если параметр отсутствует
+    // Ошибка, если параметр отсутствует
     echo json_encode(array('error'=> 'Missing user_id parameter'));
     exit;
 }
 
-// Получаю значение параметра user_id
+// Получение значения параметра user_id
 $user_id = $_GET['user_id'];
 
-// Устанавливаю параметры подключения к базе данных
+// Параметры подключения к базе данных
 $host = 'database_host';
 $dbname = 'database_name';
 $username = 'database_username';
 $password = 'database_password';
 
 try {
-    // Устанавливаю соединение с базой данных
+    // Соединение с базой данных
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-    // Подготавливаю SQL-запрос для получения баланса пользователя
+    // SQL-запрос для получения баланса пользователя
     $query = "SELECT SUM(amount) AS balance FROM transactions WHERE paid_by = :user_id OR paid_to = :user_id";
     $statement = $pdo->prepare($query);
     $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
 
-    // Выполняю запрос
+    // Запрос
     $statement->execute();
 
-    //Получаю результат
+    //Получение результата
     $result = $statement->fetch(PDO::FETCH_ASSOC);
 
-    //Проверяю наличие результата
+    //Проверка наличия результата
     if ($result) {
-        //Возвращаю баланс пользователя
+        //Баланс пользователя
         echo json_encode(['balance' => $result['balance']]);
         } else {
             echo json_encode(['error'=> 'User not found']);
         }
 } catch (PDOException $e) {
-    //Возвращаю ошибку при возникновении исключения
+    //Ошибка при возникновении исключения
     echo json_encode(['error'=> $e->getMessage()]);
 }
 ?>
